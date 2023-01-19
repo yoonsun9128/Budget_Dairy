92089643fdb007ab9c4633a435b27c1fe20d3f26
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-19 19:20:50 +0900

    [ADD] user 모델 생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        if not name:
+            raise ValueError('must have user name')
+        user = self.model(
+            email = self.normalize_email(email),
+            name = name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name=None, password=None):
+        user = self.create_user(
+            email,
+            password = password,
+            name = name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(default='', max_length=100, unique=True)
+    name = models.CharField(default='', max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
