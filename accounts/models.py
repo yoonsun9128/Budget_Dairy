e214c39043f8a83e45113b0a42abbcf8b0d8c19f
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-19 21:22:22 +0900

    [ADD] account model 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Account(models.Model):
+    Payment_Method = (
+        ("카드","카드"),
+        ("현금","현금"),
+        ("이체","이체"),
+        ("입금","입금")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField(verbose_name="금액")
+    memo = models.CharField(max_length=50)
+    method = models.CharField(max_length=50, choices=Payment_Method)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
